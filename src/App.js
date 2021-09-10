8ec52f27448f4326d57f83abb45df5771dcb99a9
zune2222 <zun_e@kakao.com>
2021-09-11 03:23:32 +0900

    0911-03:23

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route } from 'react-router-dom';
 import Home from './Home';
 import SignUp from './SignUp';
 import favoriteTaxi from './favoriteTaxi';
-import signin from "./signIn";
 const App = () => {
   return (
     <div>
       <Route path="/" exact={true} component={SignUp} />
       <Route path="/home" component={Home} />
       <Route path="/favoriteTaxi" component={favoriteTaxi} />
-      <Route path="/signin" component={signin}/>
     </div>
   );
 };
